b6b9b154d8c1eeb88599c6071d5deb055afd7330
Aaron Bieber <aaron@bolddaemon.com>
2025-06-27 12:39:00 -0600

    flake: update
    v0.1.3

--- flake.nix
+++ flake.nix
@@ -4,62 +4,77 @@
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
   outputs =
-    { self
-    , nixpkgs
-    ,
+    {
+      self,
+      nixpkgs,
     }:
     let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
     in
     {
       overlay = _: prev: { inherit (self.packages.${prev.system}) fynado; };
 
-      packages = forAllSystems (system:
+      packages = forAllSystems (
+        system:
         let
           pkgs = nixpkgsFor.${system};
         in
         {
-          fynado = with pkgs; pkgs.buildGoModule rec {
-            pname = "fynado";
-            version = "v0.1.2";
-            src = ./.;
+          fynado =
+            with pkgs;
+            pkgs.buildGoModule rec {
+              pname = "fynado";
+              version = "v0.1.3";
+              src = ./.;
 
-            vendorHash = "sha256-J1utJvNdNidZGUUz5URuyjAvf5+SSYkUlLSQqaUlyF0=";
+              vendorHash = "sha256-J1utJvNdNidZGUUz5URuyjAvf5+SSYkUlLSQqaUlyF0=";
 
-            nativeBuildInputs = [ pkg-config copyDesktopItems ];
-            buildInputs = [
-              fyne
-              glfw
-              libGL
-              libGLU
-              openssh
-              pkg-config
-              glibc
-              xorg.libXcursor
-              xorg.libXi
-              xorg.libXinerama
-              xorg.libXrandr
-              xorg.libXxf86vm
-              xorg.xinput
-            ];
+              nativeBuildInputs = [
+                pkg-config
+                copyDesktopItems
+              ];
+              buildInputs = [
+                fyne
+                glfw
+                libGL
+                libGLU
+                openssh
+                pkg-config
+                glibc
+                xorg.libXcursor
+                xorg.libXi
+                xorg.libXinerama
+                xorg.libXrandr
+                xorg.libXxf86vm
+                xorg.xinput
+                wayland
+                libxkbcommon
+              ];
 
-            buildPhase = ''
-              ${fyne}/bin/fyne package
-            '';
+              buildPhase = ''
+                ${fyne}/bin/fyne package
+              '';
 
-            installPhase = ''
+              installPhase = ''
                 mkdir -p $out
                 pkg="$PWD/${pname}.tar.xz"
                 cd $out
                 tar --strip-components=1 -xvf $pkg
-             '';
-          };
-        });
+              '';
+            };
+        }
+      );
 
       defaultPackage = forAllSystems (system: self.packages.${system}.fynado);
-      devShells = forAllSystems (system:
+      devShells = forAllSystems (
+        system:
         let
           pkgs = nixpkgsFor.${system};
         in
@@ -88,10 +103,13 @@
               xorg.libXxf86vm
               xorg.xinput
               graphviz
+              wayland
+              libxkbcommon
 
               go-font
             ];
           };
-        });
+        }
+      );
     };
 }
